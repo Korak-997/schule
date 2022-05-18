ac907c6454dea4eff2288f17259400031ad2bb40
Korak Kurani <59537065+Korak-997@users.noreply.github.com>
2022-05-18 21:50:44 +0200

    Add files via upload

---
+++ LF08/mathe/index.php
@@ -0,0 +1,169 @@
+<?php
+  $startTime = "";
+  $showSetup = true;
+  $showQuiz = false;
+  $showResult = false;
+  $err = "";
+  $json = json_decode(file_get_contents("./data.json"),TRUE);
+
+  function getMinMax($level){
+    if($level == "leicht") return [1,10];
+    if($level == "mittel") return [21,40];
+    if($level == "schwer") return [41,70];
+  }
+  if(isset($_POST['submit-setup'])) {
+    $startTime = microtime(true);
+    $showSetup = false;
+    $showQuiz = true;
+    $setup = [
+      "difficulty" => $_POST['level'],
+      "variants" => $_POST['variant'],
+    ];
+    $json["setup"] = $setup;
+    $questions = [];
+    $minMax= getMinMax($setup["difficulty"]);
+    for($i = 0; $i < $setup["variants"]; $i++){
+      array_push($questions, create_question($i, $minMax));
+    }
+    $json["questions"] = $questions;
+    file_put_contents("./data.json", json_encode($json));
+  }
+
+  if(isset($_POST['submit-answers'])){
+    $index = 0;
+    $showSetup = false;
+    $showQuiz = false;
+    $questions = $json["questions"];
+    $results = [];
+    foreach($questions as $key => $question){
+      $answer = preg_match("/[a-zA-Z]/i", $_POST[(int)$key]) ? "gültige Zahl eingeben !" : $_POST[(int)$key];
+      array_push($results, [
+        "key" => $key,
+        "question" => $question["question"],
+        "answer" => $answer,
+        "correctAnswer" => $question["answer"],
+        "isCorrect" => $question["answer"] == $answer
+      ]);
+    }
+    $json["results"] = $results;
+    file_put_contents("./data.json", json_encode($json));
+    $showResult = true;
+  }
+
+  function create_question($index, $minMax){
+    $ops = ["+", "-", "/", "*"];
+    $randOp = array_rand($ops, 1);
+    if($ops[$randOp] == "+" || $ops[$randOp] == "*"){
+      $num1 = rand($minMax[0], $minMax[1]);
+      $num2 = rand($minMax[0], $minMax[1]);
+      $answer = $ops[$randOp] == "+" ? $num1 + $num2 : $num1 * $num2;
+      return [
+        "key"=> $index,
+        "question" => "$num1 $ops[$randOp] $num2",
+        "answer" => $answer
+      ];
+    }elseif($ops[$randOp] == "-" || $ops[$randOp] == "/"){
+      $num2 = rand($minMax[0], $minMax[1]);
+      $answer = rand($minMax[0], $minMax[1]);
+      $num1 = $ops[$randOp] == "-" ? $answer + $num2 : $answer * $num2;
+      return [
+        "key" => $index,
+        "question"=> "$num1 $ops[$randOp] $num2",
+        "answer"=> $answer ];
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MATH</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+  <h1> Mathe-Aufgaben </h1>
+<?php
+  if(isset($json["setup"]["difficulty"])){
+    echo "<h1>Level : "
+      .$json['setup']['difficulty']
+      ."</h1>";
+  }
+  if($showSetup): ?>
+  <form action="index.php" method="POST">
+    <div>
+      <label for="level">Level</label>
+      <select name="level" id="level">
+        <option value="leicht">Leicht</option>
+        <option value="mittel">Mittel</option>
+        <option value="schwer">Schwer</option>
+      </select>
+    </div>
+    <div>
+      <label for="variant">Anzahl</label>
+      <select name="variant" id="variant">
+        <option value="4">4</option>
+        <option value="6">6</option>
+        <option value="8">8</option>
+      </select>
+    </div>
+      <input type="submit" name="submit-setup" value="Zu den Aufgaben">
+  </form>
+<?php
+elseif($showQuiz):
+  if($err){
+    echo "<p>$err</p>";
+  }
+?>
+<form action="index.php" method="POST">
+  <?php
+    foreach($questions as $question){
+      $k = $question['key'];
+      echo "<div>"
+        ."<label for=$i>"
+        .$question["question"]
+        ." = "
+        ."<input type='text' name=$k id=question-$k>"
+        ."</label>"
+        ."</div>";
+    }
+  ?>
+  <input type='submit' name='submit-answers' value='Auswerten'>
+</form>
+<?php
+elseif($showResult):
+  $v = $json["setup"]["variants"];
+  $results = $json["results"];
+  $score = 0;
+  foreach($results as $result){
+    if($result["isCorrect"]){
+      $score++;
+      echo "<p class='green-txt'>"
+        .$result['question']
+        ." = "
+        .$result['answer']
+        ."</p>";
+    }else{
+      echo "<p class='red-txt'>"
+      .$result['question']
+      ." = "
+      .$result['answer']
+      ."<span class='red-txt'>"
+      ."Richtige Antwort = "
+      .$result['correctAnswer']
+      ."</span>"
+      ."</p>";
+    }
+  }
+  $totalSeconds = round(microtime(true) - (int)$startTime, 2);
+  echo "<p> Sie haben $score von $v richtig beantwortet </p>";
+  echo "<p>Sie haben $totalSeconds Sekunden für die Lösungen benötigt.</p>";
+  echo "<a href='index.php?restart'>Neu Aufgaben Lösen</a>";
+
+endif;
+?>
+</body>
+</html>
